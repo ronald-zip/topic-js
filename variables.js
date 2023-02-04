51c2d2362bb1ab300943ba28caebfdac166a41a8
ronald-zip <sincheleivaronald@gmail.com>
2023-02-04 00:26:04 -0500

    Adding variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+// Declare a variable
+var nums;
+let data;
+
+// Inicialize variables 
+nums=5;
+data=12;
+
+// Add operation with data values
+var suma;
+
+suma = data + nums;
+
+console.log(suma)
